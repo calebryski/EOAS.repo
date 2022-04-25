ffe09d9efca14b51e7ec5974f5dfa9da47e27d8a
calebryski <caleb.ryski@gmail.com>
2022-04-24 23:16:45 -0400

    include 10 variables to hourly DF

--- hourly add columns.R
+++ hourly add columns.R
@@ -10,7 +10,14 @@ df1 <- read.csv("041222.datasummary_GPS_AX3.AC01.csv")
 
 df1$hours <- str_sub(df1$hour,-8,-7)
 
-dfx <- merge(x = df1, y = df2[ , c("u_id", "bs_days", "c_type")], by = "u_id", all.x = TRUE)
+head(df1)
+
+dfx <- merge(x = df1, y = df2[ , c("u_id", "c_type", 
+                                   "bs_days", "age", "age_cat", 
+                                   "n_female", "n_open0", "n_bull", 
+                                   "gpcln_bin", "axcln_bin", "lame")], 
+             
+             by = "u_id", all.x = TRUE)
 
 
 
